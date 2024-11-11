385dfcad63786775037651a3ba778b2e4462c5d2
sanskarchoudhry <65328977+sanskarchoudhry@users.noreply.github.com>
2024-11-11 16:34:37 +0530

    Delete .history directory

--- .history/src/app/(marketing)/_components/NavBar_20241111162908.tsx
+++
@@ -1,7 +0,0 @@
-export function NavBar() {
-  return (
-    <header>
-      <nav className="flex items-center gap-10 container"></nav>
-    </header>
-  );
-}
--- .history/src/app/(marketing)/layout_20241111162006.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export function MarketingLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="selection:bg-[hsl(320,65%,52%,20%)]">
-      {/* <NavBar/> */}sdfgj
-    </div>
-  );
-}
