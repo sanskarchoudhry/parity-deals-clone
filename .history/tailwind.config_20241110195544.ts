2c95d6af84d289647912aa4713d8877f358e2eb7
sanskarchoudhry <sanskar22101@gmail.com>
2024-11-11 16:32:35 +0530

    installed shadcn first commit

---
+++ .history/tailwind.config_20241110195544.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
---
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -0,0 +1,7 @@
+export function NavBar() {
+  return (
+    <header>
+      <nav className="flex items-center gap-10 container font-semibold"></nav>
+    </header>
+  );
+}
