a8b537b270b22c526d563ee5b4c7939188400302
sanskarchoudhry <sanskar22101@gmail.com>
2024-11-11 20:51:14 +0530

    added homepage

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -5,6 +5,7 @@ export default function MarketingLayout({ children }: { children: ReactNode }) {
   return (
     <div className="selection:bg-[hsl(320,65%,52%,20%)]">
       <NavBar />
+      {children}
     </div>
   );
 }
