1079ad245f664e271d91f62767df0ebc060fde7c
sanskarchoudhry <sanskar22101@gmail.com>
2024-11-23 16:50:05 +0530

    updated clerk auth for react version

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect();
+    await auth().protect();
   }
 });
 
